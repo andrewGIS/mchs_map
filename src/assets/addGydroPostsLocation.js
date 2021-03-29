9c420136c0e5f3bbfcdec5af7e0a568eb95ae659
Mister A <andrew.tarasov1993@gmail.com>
2021-03-29 16:29:05 +0500

    add esimo location

---
+++ src/assets/addGydroPostsLocation.js
@@ -0,0 +1,271 @@
+export const addHydroPosts = {
+  type: "FeatureCollection",
+  name: "add_data_valid_selection_PK",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        m4311: "59.16",
+        m4312: "57.15",
+        m4202: "УСТЬ-ИГУМ",
+        m4200: "76130",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ЯЙВА"
+      },
+      geometry: { type: "Point", coordinates: [57.15, 59.16] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "57.46",
+        m4312: "56.78",
+        m4202: "ЖИЛИНО",
+        m4200: "76702",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.БАБКА"
+      },
+      geometry: { type: "Point", coordinates: [56.78, 57.46] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "60.48",
+        m4312: "55.93",
+        m4202: "БОНДЮГ",
+        m4200: "76008",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.КАМА"
+      },
+      geometry: { type: "Point", coordinates: [55.93, 60.48] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "60.43",
+        m4312: "57.1",
+        m4202: "МИТРАКОВА",
+        m4200: "76087",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ВИШЕРА"
+      },
+      geometry: { type: "Point", coordinates: [57.1, 60.43] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "59.51",
+        m4312: "56.35",
+        m4202: "ОЩЕПКОВО",
+        m4200: "76136",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.КОНДАС"
+      },
+      geometry: { type: "Point", coordinates: [56.35, 59.51] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "59.21",
+        m4312: "54.88",
+        m4202: "ОШИБ",
+        m4200: "76147",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ВЕЛВА"
+      },
+      geometry: { type: "Point", coordinates: [54.88, 59.21] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "59.95",
+        m4312: "55.05",
+        m4202: "КОСА",
+        m4200: "76075",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.КОСА"
+      },
+      geometry: { type: "Point", coordinates: [55.05, 59.95] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "60.31",
+        m4312: "54.33",
+        m4202: "ГАЙНЫ",
+        m4200: "76007",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.КАМА"
+      },
+      geometry: { type: "Point", coordinates: [54.33, 60.31] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "57.46",
+        m4312: "56.91",
+        m4202: "СЫЛВЕНСК",
+        m4200: "76701",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.СЫЛВА"
+      },
+      geometry: { type: "Point", coordinates: [56.91, 57.46] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "57.86",
+        m4312: "58.66",
+        m4202: "КЫН",
+        m4200: "76180",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ЧУСОВАЯ"
+      },
+      geometry: { type: "Point", coordinates: [58.66, 57.86] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "58.28",
+        m4312: "54.91",
+        m4202: "КАРАГАЙ",
+        m4200: "76159",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ОБВА"
+      },
+      geometry: { type: "Point", coordinates: [54.91, 58.28] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "59.48",
+        m4312: "57.43",
+        m4202: "КАМЕНЬ",
+        m4200: "76133",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: null
+      },
+      geometry: { type: "Point", coordinates: [57.43, 59.48] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "60.33",
+        m4312: "56.5",
+        m4202: "РЯБИНИНО",
+        m4200: "76089",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ВИШЕРА"
+      },
+      geometry: { type: "Point", coordinates: [56.5, 60.33] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "58.68",
+        m4312: "57.66",
+        m4202: "УСЬВА",
+        m4200: "76685",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.УСЬВА"
+      },
+      geometry: { type: "Point", coordinates: [57.66, 58.68] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "58.71",
+        m4312: "56.76",
+        m4202: "ПЕРЕМСКОЕ",
+        m4200: "76690",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.КОСЬВА"
+      },
+      geometry: { type: "Point", coordinates: [56.76, 58.71] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "60.4",
+        m4312: "56.51",
+        m4202: "ЧЕРДЫНЬ",
+        m4200: "76112",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.КОЛВА"
+      },
+      geometry: { type: "Point", coordinates: [56.51, 60.4] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "58.3",
+        m4312: "57.71",
+        m4202: "ЛЯМИНО",
+        m4200: "76190",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ЧУСОВАЯ"
+      },
+      geometry: { type: "Point", coordinates: [57.71, 58.3] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "59.0",
+        m4312: "54.65",
+        m4202: "КУДЫМКАР",
+        m4200: "76139",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ИНЬВА"
+      },
+      geometry: { type: "Point", coordinates: [54.65, 59.0] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "59.83",
+        m4312: "56.51",
+        m4202: "ТЮЛЬКИНО",
+        m4200: "76011",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.КАМА"
+      },
+      geometry: { type: "Point", coordinates: [56.51, 59.83] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "56.5",
+        m4312: "56.13",
+        m4202: "ЧЕРНУШКА",
+        m4200: "76494",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.БЫСТРЫЙ ТАНЫП"
+      },
+      geometry: { type: "Point", coordinates: [56.13, 56.5] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "59.35",
+        m4312: "57.33",
+        m4202: "БАЗА",
+        m4200: "76127",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ЯЙВА"
+      },
+      geometry: { type: "Point", coordinates: [57.33, 59.35] }
+    },
+    {
+      type: "Feature",
+      properties: {
+        m4311: "58.98",
+        m4312: "55.55",
+        m4202: "СЛУДКА",
+        m4200: "76141",
+        m4107: "УРАЛЬСКОЕ УГМС",
+        m4301: "Р.ИНЬВА"
+      },
+      geometry: { type: "Point", coordinates: [55.55, 58.98] }
+    }
+  ]
+};
