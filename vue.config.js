3f791b11a9710c85ba6d799eebb559de4dcb0ffe
andrewGIS <34353589+andrewGIS@users.noreply.github.com>
2022-05-01 12:48:00 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === "production" ? "/~master/mchs_map/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/mchs_map" : "/",
   configureWebpack: {
     devtool: "source-map",
   },
